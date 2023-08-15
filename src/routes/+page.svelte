1342c15835731653acbf5b2465f9951c92b9da11
francis-tg <franc9885@gmail.com>
2023-08-15 19:05:14 +0000

    add todo header

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import TodoApp from "../components/todoApp.svelte";
+
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<TodoApp/>
+</section>
+
+
